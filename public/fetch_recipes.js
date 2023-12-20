96828cd9d76fe28944646336bcb42a39e540c700
abdomagdy0 <gotblockedxd@gmail.com>
2023-12-20 21:45:11 +0200

    edit

--- public/fetch_recipes.js
+++ public/fetch_recipes.js
@@ -1,11 +1,9 @@
-// public/script.js
-
 async function searchRecipes() {
     const query = document.getElementById('searchInput').value;
-    const response = await fetch(`/search/${query}`);
-    const recipes = await response.json();
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+    const data = await response.json();
 
-    displayRecipes(recipes);
+    displayRecipes(data.meals);
 }
 
 function getIngredientsList(recipe) {
@@ -36,10 +34,14 @@ async function getRandomRecipe() {
 
 async function searchByCategory(category) {
     try {
-        const response = await fetch(`/search/${category}`);
-        const recipes = await response.json();
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`);
+        const data = await response.json();
 
-        displayRecipes(recipes);
+        // Extract the meal details using the ids obtained from the category search
+        const mealIds = data.meals.map(meal => meal.idMeal);
+        const recipes = await Promise.all(mealIds.map(id => fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`).then(response => response.json())));
+
+        displayRecipes(recipes.map(recipe => recipe.meals[0]));
     } catch (error) {
         console.error(`Error fetching ${category} recipes:`, error.message);
     }
@@ -52,20 +54,19 @@ function displayRecipes(recipes) {
     recipes.forEach(recipe => {
         const recipeCard = document.createElement('div');
         recipeCard.innerHTML = `
-            <div class='recipe-cards'>
-                <section>
-                <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}">
-                </section>
-                <section class='recipe-info'>
-                    <h2>${recipe.strMeal}</h2>
-                    <h3>Ingredients:</h3>
-                    <ul>
-                        ${getIngredientsList(recipe)}
-                    </ul>
-                    <h2>Instructions</h2>
-                    <p>${recipe.strInstructions}</p>
-                </section>
-            </div>
+ 
+        <div class='recipe-cards'>
+        <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}">
+        <div class='recipe-info'>
+        <h1>${recipe.strMeal}</h1>
+        <h3>Ingredients:</h3>
+        <ul>
+            ${getIngredientsList(recipe)}
+        </ul>
+        <h3>Instructions:</h3>
+        <p>${recipe.strInstructions}</p>
+    </div>
+</div>
         `;
         recipeList.appendChild(recipeCard);
     });
